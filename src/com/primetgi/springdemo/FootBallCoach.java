18ff09b8ac9bc9873a4f60123adaccbcfb37a306
mifteqar <ifteqar.ahmed01@gmail.com>
2018-10-08 16:26:19 +0530

    Sec 4-28, practice activity, added FootBallCoach.java and modified .xml file accordingly

---
+++ src/com/primetgi/springdemo/FootBallCoach.java
@@ -0,0 +1,12 @@
+package com.primetgi.springdemo;
+
+public class FootBallCoach implements Coach{
+
+	@Override
+	public String getDailyWorkout() {
+		
+		return "Practice Running for 1 hour - FootBall Coach";
+	}
+
+	 
+}
