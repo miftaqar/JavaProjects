6e68dfe9fd91327c3ea95af05c7ec9be20e2ec0e
mifteqar <ifteqar.ahmed01@gmail.com>
2018-10-08 12:32:51 +0530

    Sec 4-22, Added new class TrackCoach.java

--- src/com/primetgi/springdemo/MyApp.java
+++ src/com/primetgi/springdemo/MyApp.java
@@ -5,7 +5,8 @@ public class MyApp {
 	public static void main(String[] args) {
 		
 		//create the object
-		Coach theCoach = new BaseBallCoach();
+		//Coach theCoach = new BaseBallCoach();
+		Coach theCoach = new TrackCoach();
 		
 		//use the object 
 		System.out.println(theCoach.getDailyWorkout());
---
+++ src/com/primetgi/springdemo/TrackCoach.java
@@ -0,0 +1,11 @@
+package com.primetgi.springdemo;
+
+public class TrackCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		
+		return "Run a 5k Hard Run";
+	}
+
+}
