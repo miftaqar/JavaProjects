6b6f9a16928d6fdaaa526f1aa6d57804d7abc793
mifteqar <ifteqar.ahmed01@gmail.com>
2018-10-10 12:33:49 +0530

    sec 6-46, part1, singleton scope(default)

---
+++ src/com/primetgi/springdemo/BeanScopeDemoApp.java
@@ -0,0 +1,32 @@
+package com.primetgi.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanScopeDemoApp {
+
+	public static void main(String[] args) {
+
+		// load the configuration file
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("BeanScope-applicationContext.xml");
+
+		// retrieve bean from the config file
+		Coach theCoach = context.getBean("myCoach", Coach.class);
+
+		Coach alphaCoach = context.getBean("myCoach", Coach.class);
+
+		// check if they are same
+		boolean result = (theCoach == alphaCoach);
+
+		// Print the results
+		System.out.println("\nPointing to the same object: " + result);
+
+		System.out.println("\nMemory Location for theCoach: " + theCoach);
+
+		System.out.println("\nMemory Location for alphaCoach: " + alphaCoach);
+
+		// close the context
+
+		context.close();
+	}
+
+}
