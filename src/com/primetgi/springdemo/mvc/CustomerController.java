cec18c1fa453ca4654bc619f04f67ace3f0c3f30
mifteqar <ifteqar.ahmed01@gmail.com>
2018-10-26 13:27:45 +0530

    sec 14-152, Spring MVC Form Validations, @InitBinder & StringTrimmerEditor

--- src/com/primetgi/springdemo/mvc/CustomerController.java
+++ src/com/primetgi/springdemo/mvc/CustomerController.java
@@ -2,15 +2,30 @@ package com.primetgi.springdemo.mvc;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Component;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Component
 @RequestMapping("/customer")
 public class CustomerController {
+
+	// add an initBinder...to convert trim input Strings
+	// remove leading and trailing whitespace
+	// resolve issue for our validation
+
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+		
+		binder.registerCustomEditor(String.class, stringTrimmerEditor);
+	}
 
 	@RequestMapping("/showForm")
 	public String showForm(Model model) {
@@ -22,6 +37,7 @@ public class CustomerController {
 
 	@RequestMapping("/processForm")
 	public String processForm(@Valid @ModelAttribute("customer") Customer customer, BindingResult bindingResult) {
+		System.out.println("LastName: |" + customer.getLastName() + "|");
 		if (bindingResult.hasErrors()) {
 			return "customer-form";
 		} else {
