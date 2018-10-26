6781c7e1a0b0726911420ade53ced28fa96fa4b1
mifteqar <ifteqar.ahmed01@gmail.com>
2018-10-26 16:51:28 +0530

    sec 16-158, Spring MVC Form Validations - Refactoring int to Integer

--- src/com/primetgi/springdemo/mvc/Customer.java
+++ src/com/primetgi/springdemo/mvc/Customer.java
@@ -14,9 +14,10 @@ public class Customer {
 	@Size(min = 1, message = "is required")
 	private String lastName;
 
+	@NotNull(message = "is required")
 	@Min(value = 0, message = "value must be greater than or equal to 0")
 	@Max(value = 10, message = "value must be less than or equal to 10")
-	private int freePasses;
+	private Integer freePasses;
 	
 	@Pattern(regexp="^[a-zA-z0-9]{5}",message="Only 5 characters / digits")
 	private String postalCode;
@@ -37,11 +38,11 @@ public class Customer {
 		this.lastName = lastName;
 	}
 
-	public int getFreePasses() {
+	public Integer getFreePasses() {
 		return freePasses;
 	}
 
-	public void setFreePasses(int freePasses) {
+	public void setFreePasses(Integer freePasses) {
 		this.freePasses = freePasses;
 	}
 
